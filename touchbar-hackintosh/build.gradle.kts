f30f9dce3a099c0b1ae3b33a403da848c5ef7b8b
luxfajah <167556681+luxfajah@users.noreply.github.com>
2026-09-13 19:45:38 -0300

    release: bump version to 1.0.11 (Build 12) and remove startup auto screenshot sequence

--- touchbar-hackintosh/build.gradle.kts
+++ touchbar-hackintosh/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.luxfajah.touchbar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 110
-        versionName = "1.0.10"
+        versionCode = 111
+        versionName = "1.0.11"
         signingConfig = signingConfigs.getByName("debug")
     }
 
